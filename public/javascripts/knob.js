2487b87de06f77b07d1969756cb1e9de2916c882
jeffa <jeffa@unlocalhost.com>
2015-06-10 16:38:43 -0400

    magic trigger

--- public/javascripts/knob.js
+++ public/javascripts/knob.js
@@ -5,6 +5,7 @@ $(function(){
         value: 0,
         turn : function(ratio){
             document.config.theta.value = parseInt( 360 * ratio / 90 ) * 90;
+            fetch_table();
         }
     });
 
